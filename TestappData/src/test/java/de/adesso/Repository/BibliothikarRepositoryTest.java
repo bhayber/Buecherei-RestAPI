068659c090e7a6d1514ef9b5f6bb2a6e49827ef4
bhayber <Burhan.Hayber@adesso.de>
2017-08-11 21:48:46 +0200

    Alles hochladen

---
+++ TestappData/src/test/java/de/adesso/Repository/BibliothikarRepositoryTest.java
@@ -0,0 +1,14 @@
+package de.adesso.Repository;
+
+import org.junit.Test;
+
+public class BibliothikarRepositoryTest {
+    @Test
+    public void findById() throws Exception {
+    }
+
+    @Test
+    public void findByBereich() throws Exception {
+    }
+
+}
