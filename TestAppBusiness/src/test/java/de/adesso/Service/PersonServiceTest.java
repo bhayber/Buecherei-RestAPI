c7b07cc5b0d2a271d77d06a5a364ea2adaae91f8
Burhan Hayber <Burhan.Hayber@adesso.de>
2017-10-23 13:27:13 +0200

    Alles hochladen

--- TestAppBusiness/src/test/java/de/adesso/Service/PersonServiceTest.java
+++ TestAppBusiness/src/test/java/de/adesso/Service/PersonServiceTest.java
@@ -1,50 +1,63 @@
-package de.adesso.Service;
-
-import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest()
-public class PersonServiceTest {
-
-    @Autowired
-    PersonService personService;
-
-    @Test
-    public void createPerson() throws Exception {
-
-    }
-
-    @Test
-    public void delete() throws Exception {
-    }
-
-    @Test
-    public void findPersonsByEmail() throws Exception {
-    }
-
-    @Test
-    public void updatePerson() throws Exception {
-    }
-
-    @Test
-    public void getPersonByPhone() throws Exception {
-    }
-
-    @Test
-    public void getPersonByID() throws Exception {
-    }
-
-    @Test
-    public void getPersonByNameAndEmail() throws Exception {
-    }
-
-    @Test
-    public void getPersonsByName() throws Exception {
-    }
-
-    @Test
-    public void getAllPerson() throws Exception {
-    }
-
-}
+//package de.adesso.Service;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.SpringBootConfiguration;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//import static org.assertj.core.api.Assertions.assertThat;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootConfiguration
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//public class PersonServiceTest {
+//
+//    @Autowired
+//    PersonService personService;
+//
+//    @Test
+//    public void createPerson() throws Exception {
+//
+//    }
+//
+//    @Test
+//    public void controllerContextLoads() throws Exception {
+//        assertThat(personService).isNotNull();
+//    }
+//
+//    @Test
+//    public void delete() throws Exception {
+//    }
+//
+//    @Test
+//    public void findPersonsByEmail() throws Exception {
+//    }
+//
+//    @Test
+//    public void updatePerson() throws Exception {
+//    }
+//
+//    @Test
+//    public void getPersonByPhone() throws Exception {
+//    }
+//
+//    @Test
+//    public void getPersonByID() throws Exception {
+//    }
+//
+//    @Test
+//    public void getPersonByNameAndEmail() throws Exception {
+//    }
+//
+//    @Test
+//    public void getPersonsByName() throws Exception {
+//    }
+//
+//    @Test
+//    public void getAllPerson() throws Exception {
+//    }
+//
+//}
