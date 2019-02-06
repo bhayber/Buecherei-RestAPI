c9a740ac69154e9b9cba3a96854fa0c35a85d292
Burhan Hayber <Burhan.Hayber@adesso.de>
2019-02-06 16:09:05 +0100

    Added Auditing For Entitys

--- TestAppApi/src/main/java/de/adesso/start/App.java
+++ TestAppApi/src/main/java/de/adesso/start/App.java
@@ -4,17 +4,17 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@EnableEurekaClient
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = { "de" })
 @EntityScan(basePackages = { "de" })
 @ComponentScan(basePackages = { "de" })
 @EnableTransactionManagement
+@EnableJpaAuditing
 public class App implements CommandLineRunner {
 
 	public static void main(String[] args) {
--- TestappData/src/main/java/de/adesso/model/EntityBase.java
+++ TestappData/src/main/java/de/adesso/model/EntityBase.java
@@ -1,29 +1,72 @@
 package de.adesso.model;
 
-import org.hibernate.annotations.GenericGenerator;
+import java.io.Serializable;
 
 import javax.persistence.Column;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
-import java.io.Serializable;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class EntityBase implements Serializable {
 
-    private static final long serialVersionUID = -1782169553419964841L;
+	private static final long serialVersionUID = -1782169553419964841L;
 
-    @Id
-    @GeneratedValue(generator = "uuid2")
-    @GenericGenerator(name = "uuid2", strategy = "uuid2")
-    @Column(unique = true)
-    private String id;
+	@Id
+	@GeneratedValue(generator = "uuid2")
+	@GenericGenerator(name = "uuid2", strategy = "uuid2")
+	@Column(unique = true)
+	private String id;
 
-    public String getId() {
-        return id;
-    }
+	@Column(name = "created_date", nullable = false, updatable = false)
+	@CreatedDate
+	private long createdDate;
 
-    public void setId(String id) {
-        this.id = id;
-    }
+	/**
+	 * @return the createdDate
+	 */
+	public long getCreatedDate() {
+		return createdDate;
+	}
+
+	/**
+	 * @param createdDate the createdDate to set
+	 */
+	public void setCreatedDate(long createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	@Column(name = "modified_date")
+	@LastModifiedDate
+	private long modifiedDate;
+
+	/**
+	 * @return the modifiedDate
+	 */
+	public long getModifiedDate() {
+		return modifiedDate;
+	}
+
+	/**
+	 * @param modifiedDate the modifiedDate to set
+	 */
+	public void setModifiedDate(long modifiedDate) {
+		this.modifiedDate = modifiedDate;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
 }
