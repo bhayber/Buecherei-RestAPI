85a901046d48cbe8cd4e5600e3d111845f003e45
Burhan Hayber <Burhan.Hayber@adesso.de>
2019-03-12 17:43:53 +0100

    Fix Db Problem

--- TestAppApi/src/test/java/de/adesso/Controller/KundenControllerTest.java
+++ TestAppApi/src/test/java/de/adesso/Controller/KundenControllerTest.java
@@ -11,12 +11,12 @@ import org.junit.runner.RunWith;
 import org.mockito.internal.matchers.Contains;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -26,6 +26,7 @@ import org.springframework.web.context.WebApplicationContext;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import de.adesso.Repository.KundenRepository;
 import de.adesso.model.Geschlecht;
 import de.adesso.model.Kunde;
 import de.adesso.start.App;
@@ -33,80 +34,85 @@ import de.adesso.start.App;
 //Die Integrationstests laufen in der eingebeteten Derby Datenbank
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration
-@WebAppConfiguration
-@SpringBootTest(classes = App.class)
+//@WebAppConfiguration
+@SpringBootTest(classes = App.class, webEnvironment = WebEnvironment.DEFINED_PORT)
 @Rollback
 public class KundenControllerTest {
 
-	@Autowired
-	WebApplicationContext context;
+    @Autowired
+    WebApplicationContext context;
 
-	private MockMvc mvc;
+    @Autowired
+    KundenRepository kundenRepository;
 
-	@Before
-	public void setUp() throws Exception {
-		mvc = MockMvcBuilders.webAppContextSetup(context).build();
-	}
+    private MockMvc mvc;
 
-	// create Kunde Test
-	@Test
-	public void test1() throws Exception {
-		Kunde newKunde = new Kunde();
-		newKunde.setAusweisnr("AB-CX-22-33-22-23");
-		newKunde.setName("Dörte");
-		newKunde.setEmail("Dörte@email.de");
-		newKunde.setAdresse("Siemensstrasse 12 60489 Frankfurt");
-		newKunde.setTelmobile("069 29 29 11");
-		newKunde.setGeschlecht(Geschlecht.WEIBLICH);
-		newKunde.setPassword("Test");
-		String sol = mvc
-				.perform(MockMvcRequestBuilders.request(HttpMethod.POST, "http://localhost:9000/kunde/createKunde")
-						.param("name", newKunde.getName()).param("adresse", newKunde.getAdresse())
-						.param("email", newKunde.getEmail()).param("ausweisnr", newKunde.getAusweisnr())
-						.param("geschlecht", newKunde.getGeschlecht().name()).param("gebDatum", "30/12/1984"))
-				.andReturn().getResponse().getContentAsString();
-		assertThat(sol, new Contains("Kunde succesfully created with id"));
-	}
+    @Before
+    public void setUp() throws Exception {
+	mvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
 
-	// get all Kunden Test
-	@Test
-	public void test2() throws Exception {
-		ObjectMapper mapper = new ObjectMapper();
 
-		String kundenlist = mvc.perform(get("http://localhost:9000/kunde/getall"))
-				.andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
-				.andReturn().getResponse().getContentAsString();
 
-		List<Kunde> list = mapper.readValue(kundenlist, new TypeReference<List<Kunde>>() {
-		});
+    // create Kunde Test
+    @Test
+    public void test1() throws Exception {
+	Kunde newKunde = new Kunde();
+	newKunde.setAusweisnr("AB-CX-22-33-22-23");
+	newKunde.setName("Dörte");
+	newKunde.setEmail("Dörte@email.de");
+	newKunde.setAdresse("Siemensstrasse 12 60489 Frankfurt");
+	newKunde.setTelmobile("069 29 29 11");
+	newKunde.setGeschlecht(Geschlecht.WEIBLICH);
+	newKunde.setPassword("Test");
+	String sol = mvc
+		.perform(MockMvcRequestBuilders.request(HttpMethod.POST, "http://localhost:9000/kunde/createKunde")
+			.param("name", newKunde.getName()).param("adresse", newKunde.getAdresse())
+			.param("email", newKunde.getEmail()).param("ausweisnr", newKunde.getAusweisnr())
+			.param("geschlecht", newKunde.getGeschlecht().name()).param("gebDatum", "30/12/1984"))
+		.andReturn().getResponse().getContentAsString();
+	assertThat(sol, new Contains("Kunde succesfully created with id"));
+    }
 
-		System.out.println("Die Liste aller Kunden (name + ausweisnr): ");
-		list.forEach(kunde -> System.out.println(kunde.getName() + " : " + kunde.getAusweisnr()));
-		assert (list.size() > 0);
-	}
+    // get all Kunden Test
+    @Test
+    public void test2() throws Exception {
+	ObjectMapper mapper = new ObjectMapper();
 
-	// Delete Kunde By AusweisNr
-	@Test
-	public void test4() throws Exception {
+	String kundenlist = mvc.perform(get("http://localhost:9000/kunde/getall"))
+		.andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+		.andReturn().getResponse().getContentAsString();
 
-		String sol = mvc
-				.perform(MockMvcRequestBuilders.request(HttpMethod.DELETE,
-						"http://localhost:9000/kunde/deleteKundeByAusweisNr/{ausweisnr}", "AB-CX-22-33-22-23"))
-				.andReturn().getResponse().getContentAsString();
-		assertThat(sol, new Contains("Kunde succesfully deleted!"));
-	}
+	List<Kunde> list = mapper.readValue(kundenlist, new TypeReference<List<Kunde>>() {
+	});
 
-	// Get Kunde By his email Test
-	@Test
-	public void test3() throws Exception {
-		ObjectMapper mapper = new ObjectMapper();
-		String sol = mvc
-				.perform(MockMvcRequestBuilders.request(HttpMethod.GET, "http://localhost:9000/kunde/getKundebyEmail/")
-						.param("email", "Dörte@email.de"))
-				.andReturn().getResponse().getContentAsString();
-		Kunde kunde = mapper.readValue(sol, Kunde.class);
-		assertThat(kunde.getEmail(), new Contains("Dörte@email.de"));
+	System.out.println("Die Liste aller Kunden (name + ausweisnr): ");
+	list.forEach(kunde -> System.out.println(kunde.getName() + " : " + kunde.getAusweisnr()));
+	assert (list.size() > 0);
+    }
 
-	}
+    // Delete Kunde By AusweisNr
+    @Test
+    public void test4() throws Exception {
+
+	String sol = mvc
+		.perform(MockMvcRequestBuilders.request(HttpMethod.DELETE,
+			"http://localhost:9000/kunde/deleteKundeByAusweisNr/{ausweisnr}", "AB-CX-22-33-22-23"))
+		.andReturn().getResponse().getContentAsString();
+	assertThat(sol, new Contains("Kunde succesfully deleted!"));
+    }
+
+    // Get Kunde By his email Test
+    @Test
+    public void test3() throws Exception {
+	ObjectMapper mapper = new ObjectMapper();
+	String sol = mvc
+		.perform(MockMvcRequestBuilders.request(HttpMethod.GET, "http://localhost:9000/kunde/getKundebyEmail/")
+			.param("email", "Dörte@email.de"))
+		.andReturn().getResponse().getContentAsString();
+	Kunde kunde = mapper.readValue(sol, Kunde.class);
+	assertThat(kunde.getEmail(), new Contains("Dörte@email.de"));
+
+    }
 
 }
--- TestappData/src/main/java/de/adesso/model/EntityBase.java
+++ TestappData/src/main/java/de/adesso/model/EntityBase.java
@@ -20,61 +20,61 @@ import de.adesso.model.EventHandler.EntityBaseEventHandler;
 @EntityListeners({ AuditingEntityListener.class, EntityBaseEventHandler.class })
 public abstract class EntityBase implements Serializable {
 
-	private static final long serialVersionUID = -1782169553419964841L;
+    private static final long serialVersionUID = -1782169553419964841L;
 
-	@Id
-	@GeneratedValue(generator = "uuid2")
-	@GenericGenerator(name = "uuid2", strategy = "uuid2")
-	@Column(unique = true)
-	private String id;
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(unique = true)
+    private String id;
 
-	@Column(name = "created_date", nullable = false, updatable = false)
-	@CreatedDate
-	private Date createdDate;
+    @Column(name = "created_date", nullable = false, updatable = false)
+    @CreatedDate
+    private Date createdDate;
 
-	@Column(name = "modified_date")
-	@LastModifiedDate
-	private Date modifiedDate;
+    @Column(name = "modified_date")
+    @LastModifiedDate
+    private Date modifiedDate;
 
-	public String getId() {
-		return id;
-	}
+    public String getId() {
+	return id;
+    }
 
-	public void setId(String id) {
-		this.id = id;
-	}
+    public void setId(String id) {
+	this.id = id;
+    }
 
-	@Override
-	public String toString() {
-		return "ID: " + id + " Erstellungsdatum: " + createdDate + " zuletzt modifiziert: " + modifiedDate;
-	}
+    @Override
+    public String toString() {
+	return "ID: " + id + " Erstellungsdatum: " + createdDate + " zuletzt modifiziert: " + modifiedDate;
+    }
 
-	/**
-	 * @return the createdDate
-	 */
-	public Date getCreatedDate() {
-		return createdDate;
-	}
+    /**
+     * @return the createdDate
+     */
+    public Date getCreatedDate() {
+	return createdDate;
+    }
 
-	/**
-	 * @param createdDate the createdDate to set
-	 */
-	public void setCreatedDate(Date createdDate) {
-		this.createdDate = createdDate;
-	}
+    /**
+     * @param createdDate the createdDate to set
+     */
+    public void setCreatedDate(Date createdDate) {
+	this.createdDate = createdDate;
+    }
 
-	/**
-	 * @return the modifiedDate
-	 */
-	public Date getModifiedDate() {
-		return modifiedDate;
-	}
+    /**
+     * @return the modifiedDate
+     */
+    public Date getModifiedDate() {
+	return modifiedDate;
+    }
 
-	/**
-	 * @param modifiedDate the modifiedDate to set
-	 */
-	public void setModifiedDate(Date modifiedDate) {
-		this.modifiedDate = modifiedDate;
-	}
+    /**
+     * @param modifiedDate the modifiedDate to set
+     */
+    public void setModifiedDate(Date modifiedDate) {
+	this.modifiedDate = modifiedDate;
+    }
 
 }
